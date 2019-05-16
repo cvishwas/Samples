57187737ce3b1d761bf2bdf21c7a6971b63b490e
vishwas <cvishwas@gmail.com>
2019-05-15 20:45:17 -0400

    Made changes to the constructor
    Made changes to the constructor

--- src/com/springboot/javabasic/Client.java
+++ src/com/springboot/javabasic/Client.java
@@ -2,10 +2,21 @@ package com.springboot.javabasic;
 
 public class Client extends Figure implements Callback {
 
+	Client(double a, double b) {
+		super(a, b);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void callback(int param) {
 		// TODO Auto-generated method stub
 		System.out.println("callback called");
+	}
+
+	@Override
+	double area() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
